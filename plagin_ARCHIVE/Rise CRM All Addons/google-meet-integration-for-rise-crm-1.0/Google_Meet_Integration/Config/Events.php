b9bbe51aa27698249b1caec9b6980d951cbfcfec
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-05-17 10:08:43 +0600

    1

--- plagin_ARCHIVE/Rise CRM All Addons/google-meet-integration-for-rise-crm-1.0/Google_Meet_Integration/Config/Events.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Google_Meet_Integration\Config;
-
-use CodeIgniter\Events\Events;
-
-Events::on('pre_system', function () {
-    helper("google_meet_integration_general");
-});
--- plagin_ARCHIVE/Rise CRM All Addons/google-meet-integration-for-rise-crm-1.0/Google_Meet_Integration/Language/french/custom_lang.php
+++
@@ -1,5 +0,0 @@
-<?php //copy from default_lang.php file and update
-
-$lang["google_meet_integration_example"] = "Example";
-
-return $lang;
